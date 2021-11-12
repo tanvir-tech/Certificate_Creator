cfe22844590d5075be693f4a1695ce298da00eb0
tanvir ahmed <tanvirah325@gmail.com>
2021-11-12 22:13:03 +0600

    pay update 234133123

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Record;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
@@ -9,4 +10,14 @@ class PaymentController extends Controller
     // pay info
     // verify payment
 
+    function verifypayment(Request $req){
+
+        $record_id = $req->record_id;
+        $transaction_id = $req->transaction_id;
+        $name = $req->transaction_reference;
+
+        // verify payment
+        // 
+
+}
 }
